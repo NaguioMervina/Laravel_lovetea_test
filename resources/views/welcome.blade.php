02f719a166f00ee0da51d2b63ecc112279787327
NaguioMervina <mervynaguio@gmail.com>
2021-04-07 21:56:03 +0800

    laravel ui auth installation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel is the best Backend framework
                 </div>
 
                 <div class="links">
