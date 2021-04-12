c51782265de0ef17ddf038fe5e2240e2c3aabefc
NaguioMervina <mervynaguio@gmail.com>
2021-04-12 14:02:02 +0800

    js and css images successful imported

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,71 @@
-@extends('layouts.app')
+@extends('layouts.front')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Welcome you are logged in') }} {{ Auth::user()->name }}!
+    <div class="electronic-banner-area">
+        <div class="custom-row-2">
+            <div class="custom-col-style-2 electronic-banner-col-3 mb-30">
+                <div class="electronic-banner-wrapper">
+                    <img src="assets/img/menu/MangoGrahams.jpg" alt="">
+                    <div class="electro-banner-style electro-banner-position">
+                    {{--  <h1>Lorem Ipsum </h1>
+                        <h2>Voulutpat</h2>
+                        <h4>lorem lorem</h4> --}}
+                    <h1><a href="product-details.html">Buy Now→</a></h1>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-col-style-2 electronic-banner-col-3 mb-30">
+                <div class="electronic-banner-wrapper">
+                    <img src="assets/img/menu/Nutella Series2.jpg" alt="">
+                    <div class="electro-banner-style electro-banner-position2">
+                     {{--   <h1>Lorem </h1>
+                        <h2>up to 15% off</h2>
+                        <h4>Lorem Ipsum</h4> --}}
+                        <a href="product-details.html">Buy Now→</a>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-col-style-2 electronic-banner-col-3 mb-30">
+                <div class="electronic-banner-wrapper">
+                    <img src="assets/img/menu/oreo series2.jpg" alt="">
+                    <div class="electro-banner-style electro-banner-position3">
+                     {{--   <h1>Lorem</h1>
+                        <h2>Super Discount</h2>
+                        <h4>lorem Ipsum</h4> --}}
+                        <a href="product-details.html">Buy Now→</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="electro-product-wrapper wrapper-padding pt-95 pb-45">
+
+        <div class="container-fluid">
+            <div class="section-title-4 text-center mb-40">
+                <h2>Top Products</h2>
+            </div>
+            <div class="top-product-style">
+
+                <div>
+                    <div id="electro1">
+                        <div class="custom-row-2">
+                           {{--
+                            @foreach($allProducts as $product)
+                                @include('product._single_product')
+
+                            @endforeach 
+
+                            --}}
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,35 +11,45 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+ <link rel="shortcut icon" type="image/png" href="/assets/img/menu/LT_logo.png ">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css">
+
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="/assets/js/jquery-3.2.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/assets/css/styling.css">
+
+ 
+   
+    
+    
+
+{{--  has problem with radio buttons
+    <link rel="stylesheet" href="/assets/css/bundle.css"> --}}
+   
+    
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+        <div class="sticky">
+            {{--
+            <div class="container1">
+                <img src="/assets/img/menu/leaf.png" class="responsive1" width="300" height="200">
+            </div> --}}
+            
+             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul>
+                @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
@@ -51,7 +61,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -61,18 +71,59 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
                         @endguest
-                    </ul>
+                        </ul>
+                <div class="dropdown1">
+                    <button class="dropbtn">Profile </button>
+                    <div class="dropdown-content">
+                        <p>
+                            <a href="account-setting-user">Account Settings</a>
+                    </div>
                 </div>
-            </div>
-        </nav>
+        
+                <a href="/error">Order Tracker</a>
+                {{--
+                <a href="{{ route('cart.index') }}">
+        
+                    @auth
+                     {{Cart::session(auth()->id())->getContent()->count()}}
+                      @else
+                      0
+                      @endauth
+        
+                    Cart
+        
+                </a>
+                --}}
+                <a href="/menu-user">Menu</a>
+                <a href="{{url('/')}}">Home</a>
+                
+                
+            </div>  
+        </div>
+        </div>
 
-        <main class="py-4">
+        {{-- display success message --}}
+        @if(session()->has('message'))
+            <div class="alert alert-success text-center" role="alert">
+               {{session('message')}}
+            </div>
+        @endif
+
+        {{-- display error message --}}
+
+        @if(session()->has('error'))
+        <div class="alert alert-danger text-center" role="alert">
+            {{session('error')}}
+        </div>
+        @endif
+
+        <main class="py-4 container">
             @yield('content')
         </main>
     </div>
